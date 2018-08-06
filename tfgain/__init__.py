5d572960fd1a17274fba23af07d6d2b2fc4922d1
Toshihiro Nakae <tnakae@users.noreply.github.com>
2018-08-06 15:40:30 +0100

    export generator and discriminator

--- tfgain/__init__.py
+++ tfgain/__init__.py
@@ -2,3 +2,5 @@ from .gain import GAIN
 from .mask import MissingMask
 from .evaluator import ImputeEvaluator
 from .data import load_breast, load_spam
+from .gain_discriminator import GAINDiscriminator
+from .gain_generator import GAINGenerator
